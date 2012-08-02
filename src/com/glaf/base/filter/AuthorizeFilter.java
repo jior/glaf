0206b2643cc0ad8eaded14dbd3a110548ef9ccd4
jior <jior2008@gmail.com>
2012-08-02 15:14:54 +0800

    update

--- src/com/glaf/base/filter/AuthorizeFilter.java
+++ src/com/glaf/base/filter/AuthorizeFilter.java
@@ -16,34 +16,53 @@ import org.apache.commons.logging.LogFactory;
 
 import com.glaf.base.modules.sys.model.SysUser;
 
-public class AuthorizeFilter implements Filter{
+public class AuthorizeFilter implements Filter {
 	private Log logger = LogFactory.getLog(AuthorizeFilter.class);
-	private String url="";
-	private String require="";
-	private String errorUrl="";
-	private String loginUser="";
-	
-	/**
-	 * 初始化Filter
-	 * @param config FilterConfig
-	 * @throws ServletException
-	 */
-	public void init(FilterConfig config) throws ServletException {
-		this.url = config.getInitParameter("login_url");
-		this.require = config.getInitParameter("require");
-		this.errorUrl = config.getInitParameter("error_url");
-		this.loginUser = config.getInitParameter("login_user");
-		logger.info("url:"+url);
-		logger.info("require:"+require);
-		logger.info("errorUrl:"+errorUrl);
-		logger.info("loginUser:"+loginUser);
-	}
-	
+	private String url = "";
+	private String require = "";
+	private String errorUrl = "";
+	private String loginUser = "";
+
 	/**
 	 * 注销
 	 */
 	public void destroy() {
 	}
+
+	/**
+	 * 过滤
+	 * 
+	 * @param request
+	 *            ServletRequest
+	 * @param response
+	 *            ServletResponse
+	 * @param chain
+	 *            FilterChain
+	 * @throws IOException
+	 * @throws ServletException
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+
+		// 需要验证
+		if ("true".equals(require)) {
+			// 检测用户是否已经登录
+			SysUser bean = (SysUser) req.getSession().getAttribute(loginUser);
+			String uri = req.getRequestURI();
+			logger.info(uri);
+
+			// 用户没有登录且当前页不是登录页面
+			logger.info("ignoreUrl:" + ignoreUrl(uri));
+			if (bean == null && !ignoreUrl(uri)) {// 显示登陆页
+				res.sendRedirect(errorUrl);
+				return;
+			}
+		}
+		chain.doFilter(req, response);
+	}
+
 	/**
 	 * 是否忽略当前页面
 	 * 
@@ -61,32 +80,22 @@ public class AuthorizeFilter implements Filter{
 		}
 		return ret;
 	}
+
 	/**
-	 * 过滤
-	 * @param request ServletRequest
-	 * @param response ServletResponse
-	 * @param chain FilterChain
-	 * @throws IOException
+	 * 初始化Filter
+	 * 
+	 * @param config
+	 *            FilterConfig
 	 * @throws ServletException
 	 */
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		HttpServletRequest req = (HttpServletRequest) request;
-		HttpServletResponse res = (HttpServletResponse) response;
-		
-		//需要验证
-		if("true".equals(require)){
-			//检测用户是否已经登录
-			SysUser bean = (SysUser) req.getSession().getAttribute(loginUser);
-			String uri = req.getRequestURI();
-			logger.info(uri);
-			
-			//用户没有登录且当前页不是登录页面
-			logger.info("ignoreUrl:" + ignoreUrl(uri));
-			if (bean==null && !ignoreUrl(uri)) {//显示登陆页
-				res.sendRedirect(errorUrl);
-				return;
-			}
-		}
-		chain.doFilter(req, response);
+	public void init(FilterConfig config) throws ServletException {
+		this.url = config.getInitParameter("login_url");
+		this.require = config.getInitParameter("require");
+		this.errorUrl = config.getInitParameter("error_url");
+		this.loginUser = config.getInitParameter("login_user");
+		logger.info("url:" + url);
+		logger.info("require:" + require);
+		logger.info("errorUrl:" + errorUrl);
+		logger.info("loginUser:" + loginUser);
 	}
 }
--- src/com/glaf/base/filter/ForbiddenCacheFilter.java
+++ src/com/glaf/base/filter/ForbiddenCacheFilter.java
@@ -10,23 +10,23 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 
-public class ForbiddenCacheFilter implements Filter{
-	private FilterConfig config = null;
-	
-	public void init(FilterConfig config) throws ServletException {
-		this.config = config;
-	}
-	
+public class ForbiddenCacheFilter implements Filter {
+	protected FilterConfig config = null;
+
 	public void destroy() {
 		config = null;
 	}
-	
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,ServletException {
-		HttpServletResponse res=(HttpServletResponse)response;
+
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		HttpServletResponse res = (HttpServletResponse) response;
 		res.setHeader("Cache-Control", "no-cache");
-		res.setHeader("Pragma", "no-cache");		
+		res.setHeader("Pragma", "no-cache");
 		res.setDateHeader("Expires", 0);
-		chain.doFilter(request, res); 
-		config.getFilterName();
+		chain.doFilter(request, res);
+	}
+
+	public void init(FilterConfig config) throws ServletException {
+		this.config = config;
 	}
 }
