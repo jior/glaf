46a02a7a60ed24cb80ecb6c43b3ecfbc4b4ba065
jior <jior2008@gmail.com>
2012-08-10 17:35:25 +0800

    update

--- src/com/glaf/base/modules/utils/ContextUtil.java
+++ src/com/glaf/base/modules/utils/ContextUtil.java
@@ -3,35 +3,34 @@ package com.glaf.base.modules.utils;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 public class ContextUtil {
-	private final static Log logger = LogFactory.getLog(ContextUtil.class);
 	private static ContextUtil instance = new ContextUtil();
-	private static Map data = new HashMap();
+	private static Map<Object, Object> dataMap = new HashMap<Object, Object>();
+
+	private ContextUtil() {
+
+	}
 
 	public static Object get(Object key) {
-		if (data.containsKey(key)) {
-			return data.get(key);
+		if (dataMap.containsKey(key)) {
+			return dataMap.get(key);
 		}
 		return null;
 	}
 
 	public static String getContextPath() {
-		return (String) data.get("__contextPath__");
+		return (String) dataMap.get("__contextPath__");
 	}
 
-	public static synchronized ContextUtil getInstance() throws Exception {
-		logger.info("getInstance");
+	public static synchronized ContextUtil getInstance() {
 		return instance;
 	}
 
 	public static void put(Object key, Object value) {
-		data.put(key, value);
+		dataMap.put(key, value);
 	}
 
 	public static void setContextPath(String contextPath) {
-		data.put("__contextPath__", contextPath);
+		dataMap.put("__contextPath__", contextPath);
 	}
 }
