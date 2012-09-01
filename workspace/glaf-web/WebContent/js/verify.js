d65a42daaab1c47ec4ba521485533e080ea6b419
jior <jior2008@gmail.com>
2012-09-01 10:10:09 +0800

    update

--- workspace/glaf-web/WebContent/js/verify.js
+++ workspace/glaf-web/WebContent/js/verify.js
@@ -1,75 +1,73 @@
 /******************************************************/
 /* 文件名：univerify.js                               */
 /* 功  能：基于自定义属性的统一检测用Javascript函数库 */
-/******************************************************/
+/** *************************************************** */
 
 /*
-================================================================================
-	功能: 是否为邮政编码
-================================================================================
-*/
+ * ================================================================================
+ * 功能: 是否为邮政编码
+ * ================================================================================
+ */
 function IsPost(str){
 	var regExp = /^[1-9]\d{5}$/g;
 	return regExp.test(str);
 }
 
 /*
-================================================================================
-	功能: 是否为电话号码
-================================================================================
-*/
+ * ================================================================================
+ * 功能: 是否为电话号码
+ * ================================================================================
+ */
 function IsPhone(str){
 	var regExp = /^((\(\d{3}\))|(\d{3}\-))?(\(0\d{2,3}\)|0\d{2,3}-)?[1-9]\d{6,7}$/g;
 	return regExp.test(str);
 }
 
 /*
-================================================================================
-	功能: 是否为手机号码
-================================================================================
-*/
+ * ================================================================================
+ * 功能: 是否为手机号码
+ * ================================================================================
+ */
 function IsMobile(str){
 	var regExp = /^((\(\d{3}\))|(\d{3}\-))?13\d{9}$/g;
 	return regExp.test(str);
 }
 
 /*
-================================================================================
-	功能: 是否为中文字符
-================================================================================
-*/
+ * ================================================================================
+ * 功能: 是否为中文字符
+ * ================================================================================
+ */
 function IsChinese(str){
 	var regExp = /^[\u0391-\uFFE5]+$/g;
 	return regExp.test(str);
 }
 
 /*
-================================================================================
-	功能: 是否为日期(yyyy[/-]mm[/-]dd)
-================================================================================
-*/
+ * ================================================================================
+ * 功能: 是否为日期(yyyy[/-]mm[/-]dd)
+ * ================================================================================
+ */
 function IsDate(str){
 	var regExp = /^(\d{1,4})(-|\/)(\d{1,2})\2(\d{1,2})$/g;
 	return regExp.test(str);
 }
 
 /*
-================================================================================
-	功能: 是否为日期(yyyy[-]mm[-]dd)
-================================================================================
-*/
+ * ================================================================================
+ * 功能: 是否为日期(yyyy[-]mm[-]dd)
+ * ================================================================================
+ */
 function IsDate(str,type){
 	var regExp = /^(\d{1,4})(-|\/)(\d{1,2})\2(\d{1,2})$/g;
 	if(type == "-")regExp = /^(\d{1,4})(-)(\d{1,2})\2(\d{1,2})$/g;
 	return regExp.test(str);
 }
 /*
-================================================================================
-	功能: 限制字符串长度
-	参数: minLen 最小长度, maxLen 最大长度(0代表没有限制)
-	返回值: boolean
-================================================================================
-*/
+ * ================================================================================
+ * 功能: 限制字符串长度 参数: minLen 最小长度, maxLen 最大长度(0代表没有限制) 返回值: boolean
+ * ================================================================================
+ */
 function LenB(str){
 	return str.replace(/[^\x00-\xff]/g,"**").length;
 }
@@ -93,12 +91,11 @@ function IsLength(str,minLen,maxLen){
 }
 
 /*
-================================================================================
-	功能: 是否为允许的上传文件类型
-	参数: limitType为允许的文件类型变量(用,分隔).如:limitType = "gif,jpg,bmp";
-	返回值: boolean
-================================================================================
-*/
+ * ================================================================================
+ * 功能: 是否为允许的上传文件类型 参数: limitType为允许的文件类型变量(用,分隔).如:limitType = "gif,jpg,bmp";
+ * 返回值: boolean
+ * ================================================================================
+ */
 function FileType(str){
 	var regExp = /\.([^\.]+)(\?|$)/;
 	if(regExp.test(str))
@@ -124,10 +121,10 @@ function IsUploadFileType(str,limitType){
 }
 
 /*
-================================================================================
-	XMLEncode(string):对字符串进行XML编码.
-================================================================================
-*/
+ * ================================================================================
+ * XMLEncode(string):对字符串进行XML编码.
+ * ================================================================================
+ */
 function XMLEncode(str){
 	str = Trim(str);
 	str = str.replace("&","&amp;");
@@ -139,10 +136,10 @@ function XMLEncode(str){
 }
 
 /*
-================================================================================
-	CheckAll(obj):复选框全选.
-================================================================================
-*/
+ * ================================================================================
+ * CheckAll(obj):复选框全选.
+ * ================================================================================
+ */
 function CheckAll(obj){
 	var obj = document.getElementsByName(obj);
 	for(var i = 0; i < obj.length; i++){
@@ -151,10 +148,10 @@ function CheckAll(obj){
 }
 
 /*
-================================================================================
-	CheckNone(obj):复选框全不选.
-================================================================================
-*/
+ * ================================================================================
+ * CheckNone(obj):复选框全不选.
+ * ================================================================================
+ */
 function CheckNone(obj){
 	var obj = document.getElementsByName(obj);
 	for(var i = 0; i < obj.length; i++)
@@ -164,10 +161,10 @@ function CheckNone(obj){
 }
 
 /*
-================================================================================
-	CheckItem(obj):复选框反选.
-================================================================================
-*/
+ * ================================================================================
+ * CheckItem(obj):复选框反选.
+ * ================================================================================
+ */
 function CheckItem(obj){
 	var obj = document.getElementsByName(obj);
 	for(var i = 0; i < obj.length; i++)	{
@@ -181,13 +178,18 @@ function CheckItem(obj){
 }
 
 /**
-================================================================================
+ * ================================================================================
  * 判断日期是否在指定日期的未来几天内.
- * @param Date appDate 开始日期.
- * @param Date futDate 结束日期.
- * @param int mixDays	至少天数.
- * @param int maxDays	最大天数.
-================================================================================
+ * 
+ * @param Date
+ *            appDate 开始日期.
+ * @param Date
+ *            futDate 结束日期.
+ * @param int
+ *            mixDays 至少天数.
+ * @param int
+ *            maxDays 最大天数.
+ *            ================================================================================
  */
 function InDate(appDate,futDate,mixDays,maxDays)
 {
@@ -223,9 +225,9 @@ function strlen(str){
   return len;
 }
 
-/* 
-检测字符串是否为空 
-*/
+/*
+ * 检测字符串是否为空
+ */
 function isNull(str){
   var i;
     
@@ -235,9 +237,9 @@ function isNull(str){
   return true;
 }
 
-/* 
-检测字符串是否全为数字 
-*/
+/*
+ * 检测字符串是否全为数字
+ */
 function isNumber(str){
   var number_chars = "1234567890";
   var i;
@@ -248,8 +250,8 @@ function isNumber(str){
   return true;
 }
 /*
-校验是否为Integer型的数据,返回的值为真和假,true和false
-*/
+ * 校验是否为Integer型的数据,返回的值为真和假,true和false
+ */
 function checkInteger(strInteger){
   if(strInteger.length==0){
     return true;
@@ -262,9 +264,9 @@ function checkInteger(strInteger){
     }
   }
 } 
-/* 
-校验是否为float型的数据,返回的值为真和假,true和false;
-*/
+/*
+ * 校验是否为float型的数据,返回的值为真和假,true和false;
+ */
 function checkFloat(strFloat){
   if(strFloat.length==0) {
     return true;
@@ -277,18 +279,18 @@ function checkFloat(strFloat){
     }
   }
 } 
-/* 
-是否小时数字格式 
-*/
+/*
+ * 是否小时数字格式
+ */
 function isHour(str){
   if(!isNumber(str)) return false;  
   var value=parseInt(str);
   if(value<0 || value>23) return false;
   return true;   
 }
-/* 
-是否分钟数字格式 
-*/
+/*
+ * 是否分钟数字格式
+ */
 function isMinute(str){
   if(!isNumber(str)) return false;  
   var value=parseInt(str);
@@ -296,8 +298,8 @@ function isMinute(str){
   return true;   
 }
 /*
-校验是否为email型的数据,返回的值为真和假,true和false;
-*/
+ * 校验是否为email型的数据,返回的值为真和假,true和false;
+ */
 function isEmail(email){
   var pattern = /[a-zA-Z0-9_.]{1,}@[a-zA-Z0-9_]{1,}.[a-zA-Z0-9_]{1,}/;
   if (email.length==0) return true;
@@ -308,21 +310,19 @@ function isEmail(email){
   }
 } 
 /*
-判断是否是日期型字符串
-用法：isDate("2002-1-31")
-*/
+ * 判断是否是日期型字符串 用法：isDate("2002-1-31")
+ */
 function isDate(str){ 
   var reg = /^(\d{1,4})(-|\/)(\d{1,2})\2(\d{1,2})$/; 
   var r = str.match(reg); 
   if(r==null)return false; 
   var d= new Date(r[1], r[3]-1,r[4]); 
-  var newStr=d.getFullYear()+r[2]+(d.getMonth()+1)+r[2]+d.getDate() 
+  var newStr=d.getFullYear()+r[2]+(d.getMonth()+1)+r[2]+d.getDate();
   return (newStr==str);
 } 
 /*
-判断是否是日期型字符串
-用法：isDateTime("2002-1-31 12:34:56")
-*/
+ * 判断是否是日期型字符串 用法：isDateTime("2002-1-31 12:34:56")
+ */
 function isDateTime(str){ 
   var reg = /^(\d{1,4})(-|\/)(\d{1,2})\2(\d{1,2}) (\d{1,2}):(\d{1,2}):(\d{1,2})$/; 
   var r = str.match(reg); 
@@ -332,16 +332,15 @@ function isDateTime(str){
   return newStr==str 
 } 
 /*
-判断日期是否正确
-用法：chkDate(2002-4-16)
-*/
+ * 判断日期是否正确 用法：chkDate(2002-4-16)
+ */
 function chkDate(sDate){ 
-  var r=/\d{4}(?:-\d{1,2}){0,2}/ 
-  //正则表达式，判断是否为yyyy-mm-dd,yyyy-mm,yyyy格式 
+  var r="/\d{4}(?:-\d{1,2}){0,2}/" ;
+  // 正则表达式，判断是否为yyyy-mm-dd,yyyy-mm,yyyy格式
   if(sDate.match(r)==sDate){ 
-　  var arr=sDate.split("-") 
+　  var arr=sDate.split("-"); 
 　  switch(arr.length){ 
-　  //根据不同的yyyy-mm-dd,yyyy-mm格式判断年月日数字是否正确 
+　  // 根据不同的yyyy-mm-dd,yyyy-mm格式判断年月日数字是否正确
 　   case 3: 
 　　   var tmpDate=new Date(arr[0],arr[1],arr[2]); 
 　　   if(tmpDate.getMonth()==arr[1] && tmpDate.getFullYear()==arr[0]) return true; 
@@ -356,8 +355,8 @@ function chkDate(sDate){
   return false; 
 } 
 /*
-判断日期DateOne是否在日期DateTwo后面
-*/
+ * 判断日期DateOne是否在日期DateTwo后面
+ */
 function compareDate(DateOne,DateTwo){
   var OneMonth = DateOne.substring(5,DateOne.lastIndexOf ("-"));
   var OneDay = DateOne.substring(DateOne.length,DateOne.lastIndexOf ("-")+1);
@@ -376,9 +375,13 @@ function compareDate(DateOne,DateTwo){
 
 /**
  * 判断日期是否在指定日期的未来几天内.
- * @param Date appDate 参考日期.
- * @param Date futDate 输入日期.
- * @param int days	天数.
+ * 
+ * @param Date
+ *            appDate 参考日期.
+ * @param Date
+ *            futDate 输入日期.
+ * @param int
+ *            days 天数.
  */
 function isInDate(appDate,futDate,days)
 {
@@ -407,7 +410,7 @@ function IsInDate(futDate)
 	return isInDate(curDate, futDate, 7);	
 }
 /**
- *判断文件类型是否正确
+ * 判断文件类型是否正确
  */
 function isFile(file, fileType){
   var arr = fileType.split(",");  
@@ -417,10 +420,10 @@ function isFile(file, fileType){
   if(start>0){
     var ext = file.substring(start+1, end);
 	ext = ext.toLowerCase( );
-	//alert(ext + ":" + fileType);
+	// alert(ext + ":" + fileType);
     for(var i=0;i<arr.length;i++){
-	  //alert(arr[i]+":"+ext);
-      if(arr[i]==ext){//类型匹配
+	  // alert(arr[i]+":"+ext);
+      if(arr[i]==ext){// 类型匹配
 		  return true;
 		  break;  
 	  }
@@ -429,10 +432,9 @@ function isFile(file, fileType){
   return false;
 }
 
-/* 
-*检测指定文本框输入是否合法
-* true验收失败，false成功 
-*/
+/*
+ * 检测指定文本框输入是否合法 true验收失败，false成功
+ */
 function verifyInput(input){
   var image;
   var i;
@@ -507,17 +509,14 @@ function verifyInput(input){
 }
 
 /**
-*校验两个日期的先后
-*返回值：
-*如果其中有一个日期为空，校验通过,          返回false
-*如果起始日期早于终止日期，校验通过，   返回true
-*如果起始日期晚于或等于终止日期，                 返回false    参考提示信息： 起始日期不能晚于结束日期。
-*/
+ * 校验两个日期的先后 返回值： 如果其中有一个日期为空，校验通过, 返回false 如果起始日期早于终止日期，校验通过， 返回true
+ * 如果起始日期晚于或等于终止日期， 返回false 参考提示信息： 起始日期不能晚于结束日期。
+ */
 function checkDateEarlier(strStart,strEnd)
 {
     if(IsDate(strStart) == false || IsDate(strEnd) == false)
         return false;
-    //如果有一个输入为空，则通过检验
+    // 如果有一个输入为空，则通过检验
     if (( strStart == "" ) || ( strEnd == "" ))
         return false;
     var arr1 = strStart.split("-");
@@ -558,13 +557,13 @@ function verifyAll(myform){
   return true;
 }
 
-/**css增加事件**/
+/** css增加事件* */
 function suckerfish(type, tag, parentId) {
 	if (window.attachEvent) {
 		window.attachEvent("onload", function() {
 			var sfEls = (parentId==null)?document.getElementsByTagName(tag):document.getElementById(parentId).getElementsByTagName(tag);
 			type(sfEls);
-			//自动调整列表宽度
+			// 自动调整列表宽度
 			if(document.getElementById("listDiv") && document.getElementById("screen")){
 			  document.getElementById("listDiv").style.width=document.getElementById("screen").clientWidth;
 			}
@@ -576,17 +575,17 @@ sfFocus = function(sfEls) {
 	for (var i=0; i<sfEls.length; i++) {
 		sfEls[i].onfocus=function() {
 			this.className+=" sffocus";
-			/*if(this.component=="currency"){//货币类型
-			  parseNum(this);
-			}*/
+			/*
+			 * if(this.component=="currency"){//货币类型 parseNum(this); }
+			 */
 		}
 		sfEls[i].onblur=function() {
 			this.className=this.className.replace(new RegExp(" sffocus\\b"), "");
-			/*if(this.component=="currency"){//货币类型
-			  parseMoney(this);
-			}*/
+			/*
+			 * if(this.component=="currency"){//货币类型 parseMoney(this); }
+			 */
 		}
-	}
+	};
 }
 
 sfTarget = function(sfEls) {
@@ -607,7 +606,7 @@ sfTarget = function(sfEls) {
 						document.lastTarget=this.targetEl;
 						return true;
 					}
-				}
+				};
 			}
 		}
 	}
@@ -618,12 +617,10 @@ suckerfish(sfFocus, "TEXTAREA");
 
 
 /**
+ * --------------------------------------------------------------------- 修改人：kxr
+ * 修改日期:2010-06-21 描述：原来的不可能输入负数，主要修改了对负数输入的准输
  * ---------------------------------------------------------------------
- * 修改人：kxr
- * 修改日期:2010-06-21
- * 描述：原来的不可能输入负数，主要修改了对负数输入的准输
- * ---------------------------------------------------------------------
- **/
+ */
 function LimitNumberInput(){
 	if (!(((window.event.keyCode >= 48) && (window.event.keyCode <= 57) ) || (window.event.keyCode == 13) ||
 			  (window.event.keyCode == 45) || (window.event.keyCode == 46)) 
@@ -650,9 +647,8 @@ function LimitNumberInput(){
 } 
 
 /*
- *parent为父对象如form
- *tag为类型，如input
-*/
+ * parent为父对象如form tag为类型，如input
+ */
 function attachLimitNumEvent(tag, parent){
   if (window.attachEvent) {
     window.attachEvent("onload", function() {
@@ -669,19 +665,14 @@ function attachLimitNumEvent(tag, parent){
 attachLimitNumEvent("INPUT");
 
 
-//数字四舍五入, 直接用toFixed()
+// 数字四舍五入, 直接用toFixed()
 Number.prototype.toFixed=function(n){
   with(Math) return round(Number(this)*pow(10, n))/pow(10, n);
   /*
-  var add = 0;
-  var s, temp;
-  var s1 = this + "";
-  var start = s1.indexOf(".");
-  if(s1.substr(start+len+1,1)>=5) add=1;
-  var temp = Math.pow(10,len);
-  s = Math.floor(this * temp) + add;
-  return s/temp;
-  */
+	 * var add = 0; var s, temp; var s1 = this + ""; var start =
+	 * s1.indexOf("."); if(s1.substr(start+len+1,1)>=5) add=1; var temp =
+	 * Math.pow(10,len); s = Math.floor(this * temp) + add; return s/temp;
+	 */
 }
 
 function parseCurrency(num){
@@ -698,4 +689,4 @@ function parseMoney(obj){
 }
 function parseNum(obj){
   obj.value=obj.currency;
-}
+};
