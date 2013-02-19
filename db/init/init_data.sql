796baef109c8e8654bb75ed57a2abb1dc8678a08
jior <jior2008@gmail.com>
2013-02-19 17:12:48 +0800

    update

--- db/init/init_data.sql
+++ db/init/init_data.sql
@@ -56,27 +56,28 @@ insert into sys_user (id, deptId, account, password, code, name, blocked, create
 insert into sys_user (id, deptId, account, password, code, name, blocked, createTime, lastLoginTime, lastLoginIP, mobile, email, telephone, evection, gender, headship, userType, fax, accountType, dumpFlag, adminFlag) values (2, 6, 'admin', 'lueSGJZetyySpUndWjMB', 'admin', 'admin', 0, '2006-07-16 16:41:52', '2009-07-08 10:27:05', '127.0.0.1', '111', 'admin@127.0.0.1', '111', 0, 0, '担当', 40, null, 0, 0, '1');
 
 /*初始化应用信息*/
+insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (3, '应用模块', '应用模块', '', 3, 1, 3);
 insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (8, '系统管理', '系统管理', '', 5, 1, 8);
+insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (9, '基础数据', '基础数据', '', 30, 1, 9);
+insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (10, '安全设置', '', '', 14, 2, 10);
 insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (11, '系统目录', '', '/sys/tree.do?method=showMain', 10, 2, 11);
 insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (12, '部门管理', '', '/sys/department.do?method=showFrame', 20, 2, 12);
 insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (14, '角色管理', '', '/sys/role.do?method=showList', 15, 2, 14);
 insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (15, '模块管理', '', '/sys/application.do?method=showFrame', 12, 2, 15);
 insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (16, '字典管理', '', '/sys/dictory.do?method=showFrame', 15, 2, 16);
-insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (276, '流程管理', '工作流管理', '', 0, 1, 434);
-insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (3, '应用模块', '应用模块', '', 3, 1, 3);
-insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (291, '工作日历', '工作日历', '/others/workCalendar.do?method=showList', 291, 1, 449);
-insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (10, '安全设置', '', '', 14, 2, 10);
 insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (20, '个人设置', '', '', 15, 2, 20);
 insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (21, '修改密码', '', '/sys/user.do?method=prepareModifyPwd', 15, 2, 21);
 insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (22, '修改个人信息', '', '/sys/user.do?method=prepareModifyInfo', 15, 2, 22);
-insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (277, '流程发布', '', '/workflow/deployController.jspa', 277, 1, 435);
-insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (278, '流程演示', '', '/workflow/test/index.jsp', 278, 1, 437);
-insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (280, '流程监控', '流程监控', '/workflow/processController.jspa?operationType=4&loadBusiness=1', 280, 1, 438);
-insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (9, '基础数据', '基础数据', '', 30, 1, 9);
 insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (26, '数据重载', '', '/sys/dictory.do?method=loadDictory', 10, 2, 26);
-insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (327, 'TODO配置', '', '/sys/todo.do?method=showList', 327, 1, 494);
-insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (315, '授权管理', '', '/sys/sysUserRole.do?method=showUsers', 315, 2, 482);
-insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (328, '调度管理', '', '/sys/scheduler.do?method=showList', 328, 1, 495);
+insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (280, '流程监控', '流程监控', '/workflow/processController.jspa?operationType=4&loadBusiness=1', 280, 1, 438);
+insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (434, '流程管理', '工作流管理', '', 0, 1, 434);
+insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (435, '流程发布', '', '/workflow/deployController.jspa', 435, 1, 435);
+insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (437, '流程演示', '', '/workflow/test/index.jsp', 437, 1, 437);
+insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (449, '工作日历', '工作日历', '/others/workCalendar.do?method=showList', 449, 1, 449);
+insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (482, '授权管理', '', '/sys/sysUserRole.do?method=showUsers', 482, 2, 482);
+insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (494, 'TODO配置', '', '/sys/todo.do?method=showList', 494, 1, 494);
+insert into sys_application (id, name, appdesc, url, sort, showmenu, nodeid) values (495, '调度管理', '', '/sys/scheduler.do?method=showList', 495, 1, 495);
+
 
 /*插入部门角色*/ 
 insert into sys_dept_role (id, grade, code, sort, sysroleid, deptid) values (1, 0, null, 0, 15, 6);
@@ -89,27 +90,29 @@ insert into sys_user_role (id, userid, roleid, authorized, authorizefrom, availd
    
  
 /*插入访问权限*/
+insert into sys_access (roleid, appid) values (1, 3);
 insert into sys_access (roleid, appid) values (1, 8);
 insert into sys_access (roleid, appid) values (1, 9);
-insert into sys_access (roleid, appid) values (1, 26);
-insert into sys_access (roleid, appid) values (1, 276);
 insert into sys_access (roleid, appid) values (1, 10);
+insert into sys_access (roleid, appid) values (1, 11);
+insert into sys_access (roleid, appid) values (1, 12);
+insert into sys_access (roleid, appid) values (1, 14);
+insert into sys_access (roleid, appid) values (1, 15);
+insert into sys_access (roleid, appid) values (1, 16);
 insert into sys_access (roleid, appid) values (1, 20);
 insert into sys_access (roleid, appid) values (1, 21);
 insert into sys_access (roleid, appid) values (1, 22);
-insert into sys_access (roleid, appid) values (1, 16);
-insert into sys_access (roleid, appid) values (1, 12);
-insert into sys_access (roleid, appid) values (1, 277);
-insert into sys_access (roleid, appid) values (1, 278);
+insert into sys_access (roleid, appid) values (1, 26);
+
 insert into sys_access (roleid, appid) values (1, 280);
-insert into sys_access (roleid, appid) values (1, 14);
-insert into sys_access (roleid, appid) values (1, 11);
-insert into sys_access (roleid, appid) values (1, 15);
-insert into sys_access (roleid, appid) values (1, 3);
-insert into sys_access (roleid, appid) values (1, 291);
-insert into sys_access (roleid, appid) values (1, 327); 
-insert into sys_access (roleid, appid) values (1, 315); 
-insert into sys_access (roleid, appid) values (1, 328); 
+insert into sys_access (roleid, appid) values (1, 434);
+insert into sys_access (roleid, appid) values (1, 435);
+insert into sys_access (roleid, appid) values (1, 437);
+
+insert into sys_access (roleid, appid) values (1, 449);
+insert into sys_access (roleid, appid) values (1, 482); 
+insert into sys_access (roleid, appid) values (1, 494); 
+insert into sys_access (roleid, appid) values (1, 495); 
 
 /*插入系统功能*/
 insert into sys_function (id, appId, name, funcDesc, funcMethod, sort) values (1, 15, '模块列表', null, 'com.glaf.base.modules.sys.action.SysApplicationAction.showList', 5);
