4de9fd0004a25c2393058aeaa76c50ceda3c4ae0
jior <jior2008@gmail.com>
2013-03-06 16:19:55 +0800

    update

--- copy-jar.bat
+++ copy-jar.bat
@@ -1,8 +1,11 @@
 cd workspace
 copy .\glaf-bi\*.jar .\glaf-web\WebContent\WEB-INF\lib
+copy .\glaf-ui\*.jar .\glaf-web\WebContent\WEB-INF\lib
 copy .\glaf-base\*.jar .\glaf-web\WebContent\WEB-INF\lib
 copy .\glaf-core\glaf-core.jar .\glaf-web\WebContent\WEB-INF\lib
 copy .\glaf-core\glaf-jbpm.jar .\glaf-web\WebContent\WEB-INF\lib
+copy .\glaf-jbpm\*.jar .\glaf-web\WebContent\WEB-INF\lib
+copy .\glaf-activiti\*.jar .\glaf-web\WebContent\WEB-INF\lib
 copy .\glaf-web\*.jar .\glaf-web\WebContent\WEB-INF\lib
 
 pause
