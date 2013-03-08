0372937f700bb87e3ac027d6039e0c904bf7b0c6
jior <jior2008@gmail.com>
2013-03-08 15:07:36 +0800

    add jar

---
+++ workspace/glaf-core/src/main/java/com/glaf/core/util/io/ByteArray.java
@@ -0,0 +1,81 @@
+/*
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.glaf.core.util.io;
+
+import static com.glaf.core.util.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class ByteArray {
+	private final byte[] bytes;
+	private final int offset;
+	private final int length;
+
+	public ByteArray(byte[] bytes) {
+		this(bytes, 0, Integer.MIN_VALUE);
+	}
+
+	public ByteArray(byte[] bytes, int offset, int length) {
+		assertNotNull(bytes, "bytes");
+		assertTrue(offset >= 0, "offset", offset);
+
+		if (length == Integer.MIN_VALUE) {
+			length = bytes.length - offset;
+		}
+
+		assertTrue(length >= 0 && length <= bytes.length - offset, "length");
+
+		this.bytes = bytes;
+		this.offset = offset;
+		this.length = length;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public int getOffset() {
+		return offset;
+	}
+
+	public byte[] getRawBytes() {
+		return bytes;
+	}
+
+	public byte[] toByteArray() {
+		byte[] copy = new byte[length];
+
+		System.arraycopy(bytes, offset, copy, 0, length);
+
+		return copy;
+	}
+
+	public InputStream toInputStream() {
+		return new ByteArrayInputStream(bytes, offset, length);
+	}
+
+	@Override
+	public String toString() {
+		return "byte[" + length + "]";
+	}
+
+	public void writeTo(OutputStream out) throws IOException {
+		out.write(bytes, offset, length);
+	}
+}
---
+++ workspace/glaf-core/src/main/java/com/glaf/core/util/threads/ThreadFactory.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.glaf.core.util.threads;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
+
+import com.glaf.core.config.BaseConfiguration;
+import com.glaf.core.config.Configuration;
+
+public class ThreadFactory {
+
+	protected final static Configuration conf = BaseConfiguration.create();
+
+	private static Executor executor = null;
+
+	static {
+		init();
+	}
+
+	private static void init() {
+		TaskQueue taskqueue = new TaskQueue();
+		TaskThreadFactory tf = new TaskThreadFactory("thread-exec-", true,
+				Thread.NORM_PRIORITY);
+		executor = new ThreadPoolExecutor(conf.getInt("ThreadPool.minThreads",
+				5), conf.getInt("ThreadPool.maxThreads", 10), 60,
+				TimeUnit.SECONDS, taskqueue, tf);
+		taskqueue.setParent((ThreadPoolExecutor) executor);
+	}
+
+	public static void run(java.lang.Runnable r) {
+		if (executor == null) {
+			init();
+		}
+		executor.execute(r);
+	}
+
+	private ThreadFactory() {
+
+	}
+
+}
