eeff23fbf3756193c8b8c4bd45adf75f7f13f7e9
jior <jior2008@gmail.com>
2013-03-22 11:13:11 +0800

    update

--- workspace/glaf-web/WebContent/WEB-INF/views/modules/identity/user/user_change_info.jsp
+++ workspace/glaf-web/WebContent/WEB-INF/views/modules/identity/user/user_change_info.jsp
@@ -35,7 +35,8 @@ function setValue(obj){
 
 <body>
 <br><br>
-<html:form action="${contextPath}/identity/user.do?method=saveModifyInfo" method="post" target="hiddenFrame" onsubmit="return checkForm(this);"> 
+<html:form action="${contextPath}/identity/user.do?method=saveModifyInfo" method="post" target="hiddenFrame" 
+           onsubmit="return checkForm(this);"> 
 <table width="500" border="0" align="center" cellpadding="0" cellspacing="0" class="box">
   <tr>
     <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
@@ -59,21 +60,25 @@ function setValue(obj){
       <tr>
         <td class="input-box2" valign="top">手　　机*</td>
         <td>
-          <input name="mobile" type="text" size="30" class="input" datatype="string" value="<%=bean.getMobile()%>" nullable="no" maxsize="12" chname="手机">        </td>
+          <input name="mobile" type="text" size="30" class="input" datatype="string" value="<%=bean.getMobile()%>" nullable="no" maxsize="12" chname="手机">        
+		</td>
       </tr>
       <tr>
         <td class="input-box2" valign="top">邮　　件*</td>
         <td>
-          <input name="email" type="text" size="30" class="input" datatype="email" value="<%=bean.getEmail()%>" nullable="no" maxsize="50" chname="邮件">        </td>
+          <input name="email" type="text" size="30" class="input" datatype="email" value="<%=bean.getEmail()%>" nullable="no" maxsize="50" chname="邮件">        
+		</td>
       </tr>
       <tr>
         <td class="input-box2" valign="top">办公电话*</td>
         <td>
-          <input name="telephone" type="text" size="30" class="input" datatype="string" value="<%=bean.getTelephone()%>" nullable="no" maxsize="20" chname="办公电话">        </td>
+          <input name="telephone" type="text" size="30" class="input" datatype="string" value="<%=bean.getTelephone()%>" nullable="no" maxsize="20" chname="办公电话">        
+		</td>
       </tr>
       <tr>
         <td colspan="2" align="center" valign="bottom" height="30">&nbsp;
-              <input name="btn_save2" type="submit" value="保存" class="button"></td>
+              <input name="btn_save2" type="submit" value="保存" class="button">
+		</td>
       </tr>
     </table></td>
   </tr>
--- workspace/glaf-web/WebContent/scripts/verify.js
+++ workspace/glaf-web/WebContent/scripts/verify.js
@@ -309,6 +309,74 @@ function isEmail(email){
     return true;
   }
 } 
+
+
+    /**
+     * Reference: Sandeep V. Tamhankar (stamhankar@hotmail.com),
+     * http://javascript.internet.com
+     */
+    function jcv_checkEmail(emailStr) {
+        if (emailStr.length == 0) {
+            return true;
+        }
+        // TLD checking turned off by default
+        var checkTLD=0;
+        var knownDomsPat=/^(com|net|org|edu|int|mil|gov|arpa|biz|aero|name|coop|info|pro|museum)$/;
+        var emailPat=/^(.+)@(.+)$/;
+        var specialChars="\\(\\)><@,;:\\\\\\\"\\.\\[\\]";
+        var validChars="\[^\\s" + specialChars + "\]";
+        var quotedUser="(\"[^\"]*\")";
+        var ipDomainPat=/^\[(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})\]$/;
+        var atom=validChars + '+';
+        var word="(" + atom + "|" + quotedUser + ")";
+        var userPat=new RegExp("^" + word + "(\\." + word + ")*$");
+        var domainPat=new RegExp("^" + atom + "(\\." + atom +")*$");
+        var matchArray=emailStr.match(emailPat);
+        if (matchArray==null) {
+            return false;
+        }
+        var user=matchArray[1];
+        var domain=matchArray[2];
+        for (i=0; i<user.length; i++) {
+            if (user.charCodeAt(i)>127) {
+                return false;
+            }
+        }
+        for (i=0; i<domain.length; i++) {
+            if (domain.charCodeAt(i)>127) {
+                return false;
+            }
+        }
+        if (user.match(userPat)==null) {
+            return false;
+        }
+        var IPArray=domain.match(ipDomainPat);
+        if (IPArray!=null) {
+            for (var i=1;i<=4;i++) {
+                if (IPArray[i]>255) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        var atomPat=new RegExp("^" + atom + "$");
+        var domArr=domain.split(".");
+        var len=domArr.length;
+        for (i=0;i<len;i++) {
+            if (domArr[i].search(atomPat)==-1) {
+                return false;
+            }
+        }
+        if (checkTLD && domArr[domArr.length-1].length!=2 && 
+            domArr[domArr.length-1].search(knownDomsPat)==-1) {
+            return false;
+        }
+        if (len<2) {
+            return false;
+        }
+        return true;
+    }
+
 /*
  * 判断是否是日期型字符串 用法：isDate("2002-1-31")
  */
@@ -485,10 +553,11 @@ function verifyInput(input){
 	  }
 	  break;  
 	case "email":
-	  if (isEmail(input.value)==false){
-		alert(input.chname+"值应该为Email类型");
-		return true;
-	  }
+	  //if (isEmail(input.value)==false){
+		if(!jcv_checkEmail(input.value)){
+		  alert(input.chname+"值应该为Email类型");
+		  return true;
+	    }
 	  break;
 	case "date":
 	  if(isNull(input.value))return false;
