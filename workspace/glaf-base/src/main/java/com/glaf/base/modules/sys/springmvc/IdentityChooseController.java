b23dabf514ea87690c7b9c4adbfdfb0edd26de4c
jior <jior2008@gmail.com>
2013-04-01 21:46:07 +0800

    update

---
+++ workspace/glaf-base/src/main/java/com/glaf/base/modules/sys/springmvc/IdentityChooseController.java
@@ -0,0 +1,266 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.glaf.base.modules.sys.springmvc;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import com.glaf.core.base.BaseTree;
+import com.glaf.core.base.TreeModel;
+import com.glaf.core.tree.helper.TreeHelper;
+import com.glaf.core.util.RequestUtils;
+import com.glaf.core.util.StringTools;
+
+import com.glaf.base.modules.sys.SysConstants;
+
+import com.glaf.base.modules.sys.model.SysDepartment;
+import com.glaf.base.modules.sys.model.SysTree;
+import com.glaf.base.modules.sys.model.SysUser;
+
+import com.glaf.base.modules.sys.service.SysDepartmentService;
+import com.glaf.base.modules.sys.service.SysTreeService;
+import com.glaf.base.modules.sys.service.SysUserService;
+
+import com.glaf.base.utils.RequestUtil;
+
+@Controller("/base/userChoose")
+@RequestMapping("/base/userChoose.do")
+public class IdentityChooseController {
+	private static final Log logger = LogFactory
+			.getLog(IdentityChooseController.class);
+
+	@javax.annotation.Resource
+	private SysDepartmentService sysDepartmentService;
+
+	@javax.annotation.Resource
+	private SysTreeService sysTreeService;
+
+	@javax.annotation.Resource
+	protected SysUserService sysUserService;
+
+	/**
+	 * 显示选择部门页面
+	 * 
+	 * @param modelMap
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping(params = "method=chooseDepts")
+	public ModelAndView chooseDepts(ModelMap modelMap,
+			HttpServletRequest request, HttpServletResponse response) {
+		RequestUtil.setRequestParameterToAttribute(request);
+		return new ModelAndView("/modules/base/choose/choose_depts", modelMap);
+	}
+
+	/**
+	 * 显示选择部门页面
+	 * 
+	 * @param modelMap
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping(params = "method=chooseTrees")
+	public ModelAndView chooseTrees(ModelMap modelMap,
+			HttpServletRequest request, HttpServletResponse response) {
+		RequestUtil.setRequestParameterToAttribute(request);
+		return new ModelAndView("/modules/base/choose/choose_trees", modelMap);
+	}
+
+	/**
+	 * 显示选择用户页面
+	 * 
+	 * @param modelMap
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping(params = "method=chooseUsers")
+	public ModelAndView chooseUsers(ModelMap modelMap,
+			HttpServletRequest request, HttpServletResponse response) {
+		RequestUtil.setRequestParameterToAttribute(request);
+		return new ModelAndView("/modules/base/choose/choose_users", modelMap);
+	}
+
+	@RequestMapping(params = "method=deptJson")
+	@ResponseBody
+	public byte[] deptJson(HttpServletRequest request, ModelMap modelMap)
+			throws IOException {
+		JSONObject result = new JSONObject();
+		String objectIds = request.getParameter("objectIds");
+		List<String> deptIds = StringTools.split(objectIds);
+		SysTree root = sysTreeService.getSysTreeByCode(SysConstants.TREE_DEPT);
+		if (root != null) {
+			logger.debug(root.toJsonObject().toJSONString());
+
+			List<TreeModel> treeModels = new ArrayList<TreeModel>();
+			// treeModels.add(root);
+			List<SysTree> trees = sysTreeService.getSysTreeListForDept(
+					(int) root.getId(), 0);
+			if (trees != null && !trees.isEmpty()) {
+				logger.debug("dept tree size:" + trees.size());
+				Map<Long, SysTree> treeMap = new HashMap<Long, SysTree>();
+				for (SysTree tree : trees) {
+					SysDepartment dept = tree.getDepartment();
+					treeMap.put(dept.getId(), tree);
+				}
+
+				for (SysTree tree : trees) {
+					if (deptIds.contains(String.valueOf(tree.getId()))) {
+						tree.setChecked(true);
+					}
+					treeModels.add(tree);
+				}
+			}
+			logger.debug("treeModels:" + treeModels.size());
+			TreeHelper treeHelper = new TreeHelper();
+			JSONArray jsonArray = treeHelper.getTreeJSONArray(treeModels);
+			return jsonArray.toJSONString().getBytes("UTF-8");
+		}
+		return result.toString().getBytes("UTF-8");
+	}
+
+	@RequestMapping(params = "method=json")
+	@ResponseBody
+	public byte[] json(HttpServletRequest request, ModelMap modelMap)
+			throws IOException {
+		JSONObject result = new JSONObject();
+		String objectIds = request.getParameter("objectIds");
+		List<String> userIds = StringTools.split(objectIds);
+		List<SysUser> users = sysUserService.getSysUserWithDeptList();
+		SysTree root = sysTreeService.getSysTreeByCode(SysConstants.TREE_DEPT);
+		if (root != null && users != null) {
+			logger.debug(root.toJsonObject().toJSONString());
+			logger.debug("users size:" + users.size());
+			List<TreeModel> treeModels = new ArrayList<TreeModel>();
+			// treeModels.add(root);
+			List<SysTree> trees = sysTreeService.getSysTreeListForDept(
+					(int) root.getId(), 0);
+			if (trees != null && !trees.isEmpty()) {
+				logger.debug("dept tree size:" + trees.size());
+				Map<Long, SysTree> treeMap = new HashMap<Long, SysTree>();
+				for (SysTree tree : trees) {
+					SysDepartment dept = tree.getDepartment();
+					treeMap.put(dept.getId(), tree);
+				}
+				long ts = System.currentTimeMillis();
+				for (SysTree tree : trees) {
+					treeModels.add(tree);
+					SysDepartment dept = tree.getDepartment();
+					if (dept != null && dept.getId() > 0) {
+						for (SysUser user : users) {
+							SysTree t = treeMap.get(Long.valueOf(user
+									.getDeptId()));
+							if (dept.getId() == user.getDeptId() && t != null) {
+								TreeModel treeModel = new BaseTree();
+								treeModel.setParentId(t.getId());
+								treeModel.setId(ts++);
+								treeModel.setCode(user.getAccount());
+								treeModel.setName(user.getAccount() + " "
+										+ user.getName());
+								treeModel.setIconCls("user");
+								if (userIds != null
+										&& userIds.contains(user.getAccount())) {
+									treeModel.setChecked(true);
+								}
+								treeModels.add(treeModel);
+							}
+						}
+					}
+				}
+			}
+			logger.debug("treeModels:" + treeModels.size());
+			TreeHelper treeHelper = new TreeHelper();
+			JSONArray jsonArray = treeHelper.getTreeJSONArray(treeModels);
+			return jsonArray.toJSONString().getBytes("UTF-8");
+		}
+		return result.toString().getBytes("UTF-8");
+	}
+
+	public void setSysDepartmentService(
+			SysDepartmentService sysDepartmentService) {
+		this.sysDepartmentService = sysDepartmentService;
+	}
+
+	public void setSysTreeService(SysTreeService sysTreeService) {
+		this.sysTreeService = sysTreeService;
+	}
+
+	public void setSysUserService(SysUserService sysUserService) {
+		this.sysUserService = sysUserService;
+	}
+
+	@RequestMapping(params = "method=treeJson")
+	@ResponseBody
+	public byte[] treeJson(HttpServletRequest request, ModelMap modelMap)
+			throws IOException {
+		JSONObject result = new JSONObject();
+		Long parentId = RequestUtils.getLong(request, "parentId");
+		String objectIds = request.getParameter("objectIds");
+		List<String> deptIds = StringTools.split(objectIds);
+		SysTree root = sysTreeService.findById(parentId);
+		if (root != null) {
+			logger.debug(root.toJsonObject().toJSONString());
+
+			List<TreeModel> treeModels = new ArrayList<TreeModel>();
+			// treeModels.add(root);
+			List<SysTree> trees = sysTreeService.getSysTreeListForDept(
+					(int) root.getId(), 0);
+			if (trees != null && !trees.isEmpty()) {
+				logger.debug("dept tree size:" + trees.size());
+				Map<Long, SysTree> treeMap = new HashMap<Long, SysTree>();
+				for (SysTree tree : trees) {
+					SysDepartment dept = tree.getDepartment();
+					treeMap.put(dept.getId(), tree);
+				}
+
+				for (SysTree tree : trees) {
+					if (deptIds.contains(String.valueOf(tree.getId()))) {
+						tree.setChecked(true);
+					}
+					treeModels.add(tree);
+				}
+			}
+			logger.debug("treeModels:" + treeModels.size());
+			TreeHelper treeHelper = new TreeHelper();
+			JSONArray jsonArray = treeHelper.getTreeJSONArray(treeModels);
+			return jsonArray.toJSONString().getBytes("UTF-8");
+		}
+		return result.toString().getBytes("UTF-8");
+	}
+
+}
