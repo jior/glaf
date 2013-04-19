b67e7fd02b0db1b994420c858348e8c4ec1293a0
jior <jior2008@gmail.com>
2013-04-19 18:28:53 +0800

    update

--- workspace/glaf-ui/src/main/java/com/glaf/ui/web/springmvc/MxPanelController.java
+++ workspace/glaf-ui/src/main/java/com/glaf/ui/web/springmvc/MxPanelController.java
@@ -30,7 +30,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.glaf.ui.model.Panel;
