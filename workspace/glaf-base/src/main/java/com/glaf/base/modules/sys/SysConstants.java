3fd62fb0814abc839249e6fb7ccea9396c67c4e4
jior <jior2008@gmail.com>
2013-04-28 16:06:05 +0800

    update

--- workspace/glaf-base/src/main/java/com/glaf/base/modules/sys/SysConstants.java
+++ workspace/glaf-base/src/main/java/com/glaf/base/modules/sys/SysConstants.java
@@ -19,29 +19,36 @@
 package com.glaf.base.modules.sys;
 
 public class SysConstants {
-	// 用户session名称
-	public static String LOGIN = "_SYS_LOGIN_USER_";
-	public static String MENU = "_SYS_LOGIN_MENU_";
-
-	public static String DEPT_LEVEL = "DeptLevel";//部门层级
-	
-	public static String USER_HEADSHIP="UserHeadship";//用户职位
-
-	public static int SORT_PREVIOUS = 0;// 前移
-	public static int SORT_FORWARD = 1;// 后移
-
-	public static int TREE_ROOT = 1;// 目录根节点
-	public static String TREE_BASE = "01";// 基础数据结构树编号
-	public static String TREE_APP = "02";// 模块结构树编号
-	public static String TREE_DICTORY = "011";// 数据字典结构树编号
-	public static String TREE_DEPT = "012";// 部门结构树编号
-
 	// 是否失效
 	public static int BLOCKED_0 = 0; // 否
+
 	public static int BLOCKED_1 = 1; // 是
+
+	public static String DEPT_LEVEL = "DeptLevel";// 部门层级代码，取值为SYS_DICTORY表的ext11
 
 	// 部门状态
 	public static Integer DEPT_STATUS_0 = Integer.valueOf(0); // 有效
+
 	public static Integer DEPT_STATUS_1 = Integer.valueOf(1); // 失效
+	// 用户session名称
+	public static String LOGIN = "SYS_LOGIN_USER";
+
+	public static String MENU = "SYS_LOGIN_MENU";
+
+	public static int SORT_FORWARD = 1;// 后移
+
+	public static int SORT_PREVIOUS = 0;// 前移
+
+	public static String TREE_APP = "02";// 模块结构树编号
+
+	public static String TREE_BASE = "01";// 基础数据结构树编号
+
+	public static String TREE_DEPT = "012";// 部门结构树编号
+
+	public static String TREE_DICTORY = "011";// 数据字典结构树编号
+
+	public static int TREE_ROOT = 1;// 目录根节点
+
+	public static String USER_HEADSHIP = "UserHeadship";// 用户职位代码，取值为SYS_DICTORY表的code
 
 }
