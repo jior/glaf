3f3183cde761f8ef6ab4227c33722ffc78768b55
jior <jior2008@gmail.com>
2013-06-09 17:46:12 +0800

    update mail bean

--- workspace/glaf-core/src/main/java/com/glaf/mail/business/MailBean.java
+++ workspace/glaf-core/src/main/java/com/glaf/mail/business/MailBean.java
@@ -1,74 +1,43 @@
 package com.glaf.mail.business;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Properties;
+
 import javax.mail.BodyPart;
 import javax.mail.Flags;
-import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Multipart;
 import javax.mail.Part;
-import javax.mail.Session;
-import javax.mail.Store;
-import javax.mail.URLName;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeUtility;
 
-import org.apache.commons.codec.binary.Hex;
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import com.glaf.core.context.ContextFactory;
-import com.glaf.core.security.DigestUtil;
 import com.glaf.core.util.FileUtils;
 import com.glaf.core.util.UUID32;
-
 import com.glaf.mail.Mail;
 import com.glaf.mail.domain.MailDataFile;
-import com.glaf.mail.service.IMailService;
 import com.glaf.mail.util.MailUtils;
 
-public class ReceiveMailBean {
+public class MailBean {
 
-	protected static final Log logger = LogFactory
-			.getLog(ReceiveMailBean.class);
+	protected static final Log logger = LogFactory.getLog(MailBean.class);
 
-	public static void main(String[] args) throws Exception {
-		ReceiveMailBean rm = new ReceiveMailBean();
-		// rm.receiveMail("pop3.163.com", 110, "cinsoft2013@163.com",
-		// "cinsoft@2013", "/temp");
-		rm.receiveMail("127.0.0.1", 110, "joy", "111111", "/temp");
-	}
-
-	private String attchSavePath = null;
 	private String dateFormat = "yyyy-MM-dd HH:mm";
+
 	private Mail mail = null;
+
 	private MimeMessage mimeMessage = null;
 
-	public ReceiveMailBean() {
-
-	}
-	
-	public ReceiveMailBean(MimeMessage mimeMessage) {
+	public MailBean(MimeMessage mimeMessage) {
 		this.mimeMessage = mimeMessage;
 		this.mail = new Mail();
-	}
-
-	public ReceiveMailBean(MimeMessage mimeMessage, Mail mail) {
-		this.mimeMessage = mimeMessage;
-		this.mail = mail;
-	}
-
-	public String getAttchSavePath() {
-		return attchSavePath;
 	}
 
 	/**
@@ -148,38 +117,6 @@ public class ReceiveMailBean {
 		}
 		mailAddr = MailUtils.convertString(mailAddr);
 		return mailAddr;
-	}
-
-	/**
-	 * 解析邮件，将得到的邮件内容保存到一个stringBuffer对象中，解析邮件 主要根据MimeType的不同执行不同的操作，一步一步的解析
-	 * 
-	 * @param part
-	 * @throws MessagingException
-	 * @throws IOException
-	 */
-	public void getMailContent(Part part) throws MessagingException,
-			IOException {
-		String contentType = part.getContentType();
-		int nameindex = contentType.indexOf("name");
-		boolean conname = false;
-		if (nameindex != -1) {
-			conname = true;
-		}
-		logger.debug("contentType:" + contentType);
-		if (part.isMimeType("text/plain") && !conname) {
-			mail.setContent((String) part.getContent());
-		} else if (part.isMimeType("text/html") && !conname) {
-			mail.setHtml((String) part.getContent());
-		} else if (part.isMimeType("multipart/*")) {
-			Multipart multipart = (Multipart) part.getContent();
-			int count = multipart.getCount();
-			for (int i = 0; i < count; i++) {
-				getMailContent(multipart.getBodyPart(i));
-			}
-		} else if (part.isMimeType("message/rfc822")) {
-			getMailContent((Part) part.getContent());
-		}
-
 	}
 
 	/**
@@ -311,8 +248,11 @@ public class ReceiveMailBean {
 		return isnew;
 	}
 
-	public void receive(Part part, int i) throws MessagingException,
-			IOException {
+	public void parse() throws MessagingException, IOException {
+		this.parse(getMimeMessage());
+	}
+
+	public void parse(Part part) throws MessagingException, IOException {
 		logger.debug("------------------START-----------------------");
 		mail.setMessageId(this.getMessageId());
 		mail.setSendDate(mimeMessage.getSentDate());
@@ -321,83 +261,19 @@ public class ReceiveMailBean {
 		mail.setMailTo(this.getMailAddress("TO"));
 		mail.setMailCC(this.getMailAddress("CC"));
 
-		logger.debug("Message" + i + " subject:" + getSubject());
-		logger.debug("Message" + i + " from:" + getFrom());
-		logger.debug("Message" + i + " isNew:" + isNew());
+		logger.debug("Message subject:" + getSubject());
+		logger.debug("Message from:" + getFrom());
+		logger.debug("Message isNew:" + isNew());
 		boolean flag = isContainAttch(part);
-		logger.debug("Message" + i + " isContainAttch:" + flag);
-		logger.debug("Message" + i + " replySign:" + getReplySign());
-		getMailContent(part);
+		logger.debug("Message isContainAttch:" + flag);
+		logger.debug("Message replySign:" + getReplySign());
+		parseMailContent(part);
 		// logger.debug("Message" + i + " content:" + getBodyText());
 
 		if (flag) {
-			saveAttachment(part);
+			parseAttachment(part);
 		}
 		logger.debug("------------------END-----------------------");
-	}
-
-	public void receiveMail(String popServer, int port, String username,
-			String password, String attachSavePath) {
-		Properties props = new Properties();
-		Session session = Session.getDefaultInstance(props, null);
-		URLName urlname = new URLName("pop3", popServer, port, null, username,
-				password);
-		Store store = null;
-		try {
-			store = session.getStore(urlname);
-			store.connect();
-			Folder folder = store.getFolder("INBOX");
-			folder.open(Folder.READ_ONLY);
-			Message msgs[] = folder.getMessages();
-			int count = msgs.length;
-			logger.debug("Message Count:" + count);
-			Mail mail = null;
-			IMailService mailService = ContextFactory.getBean("mailService");
-			logger.debug(mailService.nextId());
-			for (int i = 0; i < count; i++) {
-				MimeMessage msg = (MimeMessage) msgs[i];
-				if (mailService.getMailByMessageId(msg.getMessageID()) == null) {
-					try {
-						mail = new Mail();
-						mail.setUsername(username);
-						this.setMimeMessage(msg);
-						this.setMail(mail);
-						if (attachSavePath != null) {
-							String md5_str = DigestUtil.digestString(
-									msg.getMessageID(), "MD5");
-							String hex_str = Hex.encodeHexString(md5_str
-									.getBytes());
-							FileUtils
-									.mkdirsWithExistsCheck(new File(
-											attachSavePath + FileUtils.sp
-													+ mail.getUsername()
-													+ FileUtils.sp));
-							this.setAttchSavePath(attachSavePath + FileUtils.sp
-									+ mail.getUsername() + FileUtils.sp
-									+ hex_str);
-						}
-						this.receive(msg, i);
-						mailService.saveMail(mail);
-						logger.debug("save mail ok.");
-					} catch (Exception ex) {
-						ex.printStackTrace();
-					} finally {
-						mail = null;
-					}
-				}
-			}
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		} finally {
-			try {
-				if (store != null) {
-					store.close();
-					store = null;
-				}
-			} catch (MessagingException ex) {
-				ex.printStackTrace();
-			}
-		}
 	}
 
 	/**
@@ -407,7 +283,7 @@ public class ReceiveMailBean {
 	 * @throws MessagingException
 	 * @throws IOException
 	 */
-	public void saveAttachment(Part part) throws MessagingException,
+	public void parseAttachment(Part part) throws MessagingException,
 			IOException {
 		String filename = "";
 		if (part.isMimeType("multipart/*")) {
@@ -430,10 +306,10 @@ public class ReceiveMailBean {
 						}
 						filename = MailUtils.convertString(filename);
 						logger.debug("filename=" + filename);
-						saveFile(filename, mpart.getInputStream());
+						parseFileContent(filename, mpart.getInputStream());
 					}
 				} else if (mpart.isMimeType("multipart/*")) {
-					saveAttachment(mpart);
+					parseAttachment(mpart);
 				} else {
 					filename = mpart.getFileName();
 					if (filename != null) {
@@ -446,14 +322,14 @@ public class ReceiveMailBean {
 							filename = MimeUtility.decodeText(filename);
 						}
 						filename = MailUtils.convertString(filename);
-						saveFile(filename, mpart.getInputStream());
+						parseFileContent(filename, mpart.getInputStream());
 						logger.debug("filename=" + filename);
 					}
 				}
 			}
 
 		} else if (part.isMimeType("message/rfc822")) {
-			saveAttachment((Part) part.getContent());
+			parseAttachment((Part) part.getContent());
 		}
 	}
 
@@ -464,11 +340,10 @@ public class ReceiveMailBean {
 	 * @param inputStream
 	 * @throws IOException
 	 */
-	public void saveFile(String filename, InputStream inputStream) {
+	public void parseFileContent(String filename, InputStream inputStream) {
 		if (filename == null) {
 			return;
 		}
-		String sepatror = FileUtils.sp;
 		try {
 			filename = MailUtils.convertString(filename);
 			filename = FileUtils.replaceWin32FileName(filename);
@@ -481,28 +356,40 @@ public class ReceiveMailBean {
 			att.setSize(bytes.length);
 			att.setFileId(UUID32.getUUID());
 			mail.addFile(att);
-			if (StringUtils.isNotEmpty(getAttchSavePath())) {
-				File storeDir = new File(getAttchSavePath());
-				FileUtils.mkdirsWithExistsCheck(storeDir);
-				String storefile = getAttchSavePath() + sepatror + filename;
-				FileUtils.save(storefile, bytes);
-				logger.debug("store file's path:" + storefile.toString());
-			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
 
-	public void setAttchSavePath(String attchSavePath) {
-		this.attchSavePath = attchSavePath;
-	}
-
-	public void setMail(Mail mail) {
-		this.mail = mail;
-	}
-
-	public void setMimeMessage(MimeMessage mimeMessage) {
-		this.mimeMessage = mimeMessage;
+	/**
+	 * 解析邮件，将得到的邮件内容保存到一个stringBuffer对象中，解析邮件 主要根据MimeType的不同执行不同的操作，一步一步的解析
+	 * 
+	 * @param part
+	 * @throws MessagingException
+	 * @throws IOException
+	 */
+	public void parseMailContent(Part part) throws MessagingException,
+			IOException {
+		String contentType = part.getContentType();
+		int nameindex = contentType.indexOf("name");
+		boolean conname = false;
+		if (nameindex != -1) {
+			conname = true;
+		}
+		logger.debug("contentType:" + contentType);
+		if (part.isMimeType("text/plain") && !conname) {
+			mail.setContent((String) part.getContent());
+		} else if (part.isMimeType("text/html") && !conname) {
+			mail.setHtml((String) part.getContent());
+		} else if (part.isMimeType("multipart/*")) {
+			Multipart multipart = (Multipart) part.getContent();
+			int count = multipart.getCount();
+			for (int i = 0; i < count; i++) {
+				parseMailContent(multipart.getBodyPart(i));
+			}
+		} else if (part.isMimeType("message/rfc822")) {
+			parseMailContent((Part) part.getContent());
+		}
 	}
 
 }
