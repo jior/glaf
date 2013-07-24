dbc8457772156f543f24ad13fab5150856a7c39b
jior <jior2008@gmail.com>
2013-07-24 18:27:28 +0800

    update

---
+++ workspace/glaf-core/src/main/java/com/glaf/core/web/springmvc/MxSystemSchemaController.java
@@ -0,0 +1,94 @@
+package com.glaf.core.web.springmvc;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.glaf.core.base.ConnectionDefinition;
+import com.glaf.core.config.DBConfiguration;
+import com.glaf.core.config.Environment;
+import com.glaf.core.config.ViewProperties;
+import com.glaf.core.entity.hibernate.HibernateBeanFactory;
+import com.glaf.core.util.JsonUtils;
+import com.glaf.core.util.RequestUtils;
+import com.glaf.core.util.ResponseUtils;
+
+@Controller("/sys/schema")
+@RequestMapping("/sys/schema")
+public class MxSystemSchemaController {
+
+	@RequestMapping
+	public ModelAndView list(HttpServletRequest request, ModelMap modelMap) {
+		RequestUtils.setRequestParameterToAttribute(request);
+
+		List<ConnectionDefinition> rows = DBConfiguration
+				.getConnectionDefinitions();
+		request.setAttribute("rows", rows);
+
+		String x_query = request.getParameter("x_query");
+		if (StringUtils.equals(x_query, "true")) {
+			Map<String, Object> paramMap = RequestUtils
+					.getParameterMap(request);
+			String x_complex_query = JsonUtils.encode(paramMap);
+			x_complex_query = RequestUtils.encodeString(x_complex_query);
+			request.setAttribute("x_complex_query", x_complex_query);
+		} else {
+			request.setAttribute("x_complex_query", "");
+		}
+		String view = request.getParameter("view");
+		if (StringUtils.isNotEmpty(view)) {
+			return new ModelAndView(view, modelMap);
+		}
+
+		String x_view = ViewProperties.getString("system_schema.list");
+		if (StringUtils.isNotEmpty(x_view)) {
+			return new ModelAndView(x_view, modelMap);
+		}
+
+		return new ModelAndView("/modules/sys/schema/list", modelMap);
+	}
+
+	@ResponseBody
+	@RequestMapping("/rebuild")
+	public byte[] rebuild(HttpServletRequest request, ModelMap modelMap) {
+		RequestUtils.setRequestParameterToAttribute(request);
+		String systemName = request.getParameter("systemName");
+		Environment.setCurrentSystemName(systemName);
+		try {
+			HibernateBeanFactory.reload();
+			return ResponseUtils.responseJsonResult(true);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return ResponseUtils.responseJsonResult(false);
+	}
+
+	@ResponseBody
+	@RequestMapping("/rebuildAll")
+	public byte[] rebuildAll(HttpServletRequest request, ModelMap modelMap) {
+		RequestUtils.setRequestParameterToAttribute(request);
+		List<ConnectionDefinition> rows = DBConfiguration
+				.getConnectionDefinitions();
+		try {
+			if (rows != null && !rows.isEmpty()) {
+				for (ConnectionDefinition conn : rows) {
+					Environment.setCurrentSystemName(conn.getName());
+					HibernateBeanFactory.reload();
+				}
+			}
+			return ResponseUtils.responseJsonResult(true);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return ResponseUtils.responseJsonResult(false);
+	}
+
+}
