fc90ae0ae783119044e3f52ed4ff5b56552353f2
jior <jior2008@gmail.com>
2013-08-16 15:53:01 +0800

    update

--- workspace/glaf-web/WebContent/scripts/glaf-base.js
+++ workspace/glaf-web/WebContent/scripts/glaf-base.js
@@ -163,4 +163,17 @@ function uploadFile(referType, referId, viewType){
 	art.dialog.open(link, { height: 300, width: 500, title: "上传下载", lock: true, scrollbars:"no" }, false);
 }
 	 
- 
+/**
+解决只读框按Backspace键时IE后退的问题
+**/
+document.onkeydown = check;
+function check(e) {
+	if (!e) var e = window.event;
+	if (e.keyCode) code = e.keyCode;
+	else if (e.which) code = e.which;
+	if (((event.keyCode == 8) && ((event.srcElement.type != "text" && event.srcElement.type != "textarea" && event.srcElement.type != "password") || event.srcElement.readOnly == true)) || ((event.ctrlKey) && ((event.keyCode == 78) || (event.keyCode == 82))) || (event.keyCode == 116)) {
+		event.keyCode = 0;
+		event.returnValue = false;
+	}
+	return true;
+} 
