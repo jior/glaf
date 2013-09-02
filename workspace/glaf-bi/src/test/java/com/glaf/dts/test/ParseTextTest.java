356d9c3ac940f87e0f02945478c0bdb180ca9d89
jior <jior2008@gmail.com>
2013-09-02 16:18:21 +0800

    update

---
+++ workspace/glaf-bi/src/test/java/com/glaf/dts/test/ParseTextTest.java
@@ -0,0 +1,57 @@
+package com.glaf.dts.test;
+
+import java.util.Date;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import com.glaf.core.base.ColumnModel;
+import com.glaf.core.base.TableModel;
+import com.glaf.core.context.ContextFactory;
+import com.glaf.core.parse.ParserFacede;
+import com.glaf.core.util.DateUtils;
+
+public class ParseTextTest {
+
+	@Test
+	public void testImportText() {
+		String mappingFile = "./report/mapping/ProductFact.mapping.xml";
+		String dataFile = "./report/data/PALC-PBRT-RSLT.txt";
+		ContextFactory.hasBean("dataSource");
+		ParserFacede parser = new ParserFacede();
+		List<TableModel> rows = parser.parse(mappingFile, dataFile, "root_"
+				+ DateUtils.getDateTime("yyyyMMddHHmmss", new Date()), true);
+		if (rows != null && !rows.isEmpty()) {
+			JSONArray array = new JSONArray();
+			for (TableModel model : rows) {
+				JSONObject jsonObject = new JSONObject();
+				List<ColumnModel> columns = model.getColumns();
+				if (columns != null && !columns.isEmpty()) {
+					for (ColumnModel col : columns) {
+						if (col.getName() != null) {
+							jsonObject.put(col.getName(), col.getValue());
+						}
+						if (col.getColumnName() != null) {
+							jsonObject.put(col.getColumnName().toLowerCase(),
+									col.getValue());
+						}
+					}
+				}
+				if (model.getIdColumn() != null) {
+					ColumnModel col = model.getIdColumn();
+					if (col.getName() != null) {
+						jsonObject.put(col.getName(), col.getValue());
+					}
+					if (col.getColumnName() != null) {
+						jsonObject.put(col.getColumnName().toLowerCase(),
+								col.getValue());
+					}
+				}
+				array.put(jsonObject);
+			}
+			System.out.println(array.toString('\n'));
+		}
+	}
+}
