93c1515271a978630a7709ab1dcb1fd29e77d2db
jior <jior2008@gmail.com>
2013-09-25 09:50:28 +0800

    update

--- workspace/glaf-jbpm/src/main/java/com/glaf/jbpm/action/SqlMapTaskInstanceComplexAction.java
+++ workspace/glaf-jbpm/src/main/java/com/glaf/jbpm/action/SqlMapTaskInstanceComplexAction.java
@@ -1,20 +1,20 @@
 /*
-* Licensed to the Apache Software Foundation (ASF) under one
-* or more contributor license agreements.  See the NOTICE file
-* distributed with this work for additional information
-* regarding copyright ownership.  The ASF licenses this file
-* to you under the Apache License, Version 2.0 (the
-* "License"); you may not use this file except in compliance
-* with the License.  You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.glaf.jbpm.action;
 
@@ -269,7 +269,6 @@ public class SqlMapTaskInstanceComplexAction implements ActionHandler {
 						}
 
 						if (actors != null && actors.size() > 0) {
-
 							Iterator<Object> iterator = actors.iterator();
 							while (iterator.hasNext()) {
 								Object object = iterator.next();
@@ -405,6 +404,7 @@ public class SqlMapTaskInstanceComplexAction implements ActionHandler {
 					List<String> pieces = StringTools.split(value.toString(),
 							",");
 					params.put("deptIds", pieces);
+					params.remove("deptId");
 				}
 			}
 
@@ -423,6 +423,7 @@ public class SqlMapTaskInstanceComplexAction implements ActionHandler {
 					List<String> pieces = StringTools.split(value.toString(),
 							",");
 					params.put("roleIds", pieces);
+					params.remove("roleId");
 				}
 			}
 
