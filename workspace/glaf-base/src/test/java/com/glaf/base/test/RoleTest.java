6a5a571169f822d2c2ed06475b2add7a0e6a2e08
jior <jior2008@gmail.com>
2013-10-15 11:44:25 +0800

    update

---
+++ workspace/glaf-base/src/test/java/com/glaf/base/test/RoleTest.java
@@ -0,0 +1,30 @@
+package com.glaf.base.test;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.glaf.base.modules.sys.service.SysRoleService;
+import com.glaf.base.security.BaseIdentityFactory;
+
+public class RoleTest extends AbstractTest {
+	
+	protected SysRoleService sysRoleService;
+	
+	@Test
+	public void testUserRoles(){
+		sysRoleService = getBean("sysRoleService");
+		List<String> codes = BaseIdentityFactory.getUserRoles("root");
+		logger.debug(codes);
+	}
+
+	public SysRoleService getSysRoleService() {
+		return sysRoleService;
+	}
+
+	public void setSysRoleService(SysRoleService sysRoleService) {
+		this.sysRoleService = sysRoleService;
+	}
+	
+
+}
