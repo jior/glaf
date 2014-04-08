f50ff32c89f5bfba7e2bb16782bf4b8a66863744
jior <jior2008@gmail.com>
2014-04-08 17:29:23 +0800

    add linux shell

---
+++ workspace/glaf-transfer/run-sqlite-export.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ -x "$JAVA_HOME/bin/java" ]; then
+    JAVA="$JAVA_HOME/bin/java"
+else
+    JAVA=`which java`
+fi
+
+if [ ! -x "$JAVA" ]; then
+    echo "Could not find any executable java binary. Please install java in your PATH or set JAVA_HOME"
+    exit 1
+fi
+
+exec ./ant/bin/ant run-sqlite-export -Djvm.arg="-Xmx512m -XX:PermSize=128m -XX:MaxPermSize=256m"
