8ffd2e40ea9e3eb1a5726cbf4ec1b89de33f7697
jior <jior2008@gmail.com>
2014-04-22 15:50:27 +0800

    update shiro

---
+++ workspace/glaf-security/src/main/java/com/glaf/shiro/AnyRoleAuthorizationFilter.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.glaf.shiro;
+
+import java.io.IOException;
+import java.util.Set;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.CollectionUtils;
+import org.apache.shiro.web.filter.authz.AuthorizationFilter;
+
+/**
+ * 拥有任何一个角色即可访问
+ * 
+ */
+public class AnyRoleAuthorizationFilter extends AuthorizationFilter {
+
+	public boolean isAccessAllowed(ServletRequest request,
+			ServletResponse response, Object mappedValue) throws IOException {
+
+		Subject subject = getSubject(request, response);
+		String[] rolesArray = (String[]) mappedValue;
+
+		if (rolesArray == null || rolesArray.length == 0) {
+			return false;
+		}
+
+		Set<String> roles = CollectionUtils.asSet(rolesArray);
+		boolean isAccessAllowed = false;
+		for (String role : roles) {
+			if (subject.hasRole(role)) {
+				isAccessAllowed = true;
+				break;
+			}
+		}
+
+		return isAccessAllowed;
+	}
+
+}
