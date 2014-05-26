64145d1620d5958147e6b70080129ec773c14824
jior <jior2008@gmail.com>
2014-05-26 18:06:02 +0800

    update AuthorizeService

--- workspace/glaf-base/src/main/java/com/glaf/base/modules/sys/service/mybatis/AuthorizeServiceImpl.java
+++ workspace/glaf-base/src/main/java/com/glaf/base/modules/sys/service/mybatis/AuthorizeServiceImpl.java
@@ -22,7 +22,7 @@ import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.h2.util.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
