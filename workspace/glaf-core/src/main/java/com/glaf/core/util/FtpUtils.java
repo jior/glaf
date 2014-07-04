dbeb75d1eee4fbc23e370e39943df0a109d8e05d
jior <jior2008@gmail.com>
2014-07-04 08:57:17 +0800

    update FtpUtils

--- workspace/glaf-core/src/main/java/com/glaf/core/util/FtpUtils.java
+++ workspace/glaf-core/src/main/java/com/glaf/core/util/FtpUtils.java
@@ -18,6 +18,10 @@
 
 package com.glaf.core.util;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -147,6 +151,136 @@ public class FtpUtils {
 		}
 	}
 
+	public static byte[] getBytes(String remoteFile) {
+		byte[] bytes = null;
+		InputStream in = null;
+		ByteArrayOutputStream out = null;
+		BufferedOutputStream bos = null;
+		try {
+			// 设置被动模式
+			ftpClient.enterLocalPassiveMode();
+			// 设置以二进制方式传输
+			ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+			FTPFile[] files = ftpClient.listFiles(new String(remoteFile
+					.getBytes("GBK"), "ISO-8859-1"));
+			if (files.length != 1) {
+				logger.warn("remote file is not exists");
+				return null;
+			}
+			long lRemoteSize = files[0].getSize();
+
+			out = new ByteArrayOutputStream();
+			bos = new BufferedOutputStream(out);
+			in = ftpClient.retrieveFileStream(new String(remoteFile
+					.getBytes("GBK"), "ISO-8859-1"));
+			byte[] buff = new byte[4096];
+			long step = lRemoteSize / 100;
+			long progress = 0;
+			long localSize = 0L;
+			int c;
+			while ((c = in.read(buff)) != -1) {
+				out.write(buff, 0, c);
+				localSize += c;
+				long nowProgress = localSize / step;
+				if (nowProgress > progress) {
+					progress = nowProgress;
+					if (progress % 10 == 0) {
+						logger.debug("download progress:" + progress);
+					}
+				}
+			}
+			out.flush();
+			bos.flush();
+			bytes = out.toByteArray();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			logger.error("download error", ex);
+		} finally {
+			IOUtils.closeStream(in);
+			IOUtils.closeStream(out);
+		}
+		return bytes;
+	}
+
+	/**
+	 * 根据传输文件下载
+	 * 
+	 * @param remotePath
+	 *            FTP路径
+	 * @param remoteFile
+	 *            FTP文件
+	 * @param localFile
+	 *            本地文件全路径
+	 */
+	public static byte[] getBytes(String remotePath, String remoteFile) {
+		try {
+			if (remotePath != null && remotePath.length() != 0) {
+				ftpClient.changeWorkingDirectory(remotePath);
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			logger.error("download error", ex);
+		}
+		return getBytes(remoteFile);
+	}
+
+	/**
+	 * 根据文件路径上传
+	 * 
+	 * @param remoteFile
+	 *            FTP文件
+	 * @param input
+	 *            本地输入流
+	 */
+	public static void upload(String remoteFile, byte[] bytes) {
+		ByteArrayInputStream bais = null;
+		BufferedInputStream bis = null;
+		try {
+			ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+			ftpClient.enterLocalPassiveMode();
+			ftpClient.setFileTransferMode(FTP.STREAM_TRANSFER_MODE);
+			bais = new ByteArrayInputStream(bytes);
+			bis = new BufferedInputStream(bais);
+			boolean flag = ftpClient.storeFile(remoteFile, bis);
+			if (flag) {
+				logger.info("upload success");
+			} else {
+				logger.info("upload failure");
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			logger.error("upload error", ex);
+		} finally {
+			IOUtils.closeStream(bis);
+			IOUtils.closeStream(bais);
+		}
+	}
+
+	/**
+	 * 根据文件路径上传
+	 * 
+	 * @param remoteFile
+	 *            FTP文件
+	 * @param input
+	 *            本地文件流
+	 */
+	public static void upload(String remoteFile, InputStream input) {
+		try {
+			ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+			ftpClient.enterLocalPassiveMode();
+			ftpClient.setFileTransferMode(FTP.STREAM_TRANSFER_MODE);
+			boolean flag = ftpClient.storeFile(remoteFile, input);
+			if (flag) {
+				logger.info("upload success");
+			} else {
+				logger.info("upload failure");
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			logger.error("upload error", ex);
+		}
+	}
+
 	/**
 	 * 根据文件路径上传
 	 * 
@@ -174,6 +308,51 @@ public class FtpUtils {
 			logger.error("upload error", ex);
 		} finally {
 			IOUtils.closeStream(input);
+		}
+	}
+
+	/**
+	 * 根据文件路径上传
+	 * 
+	 * @param remotePath
+	 *            FTP路径
+	 * @param remoteFile
+	 *            FTP文件
+	 * @param bytes
+	 *            本地字节流
+	 */
+	public static void upload(String remotePath, String remoteFile, byte[] bytes) {
+		try {
+			if (remotePath != null && remotePath.length() != 0) {
+				ftpClient.changeWorkingDirectory(remotePath);
+			}
+			upload(remoteFile, bytes);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			logger.error("upload error", ex);
+		}
+	}
+
+	/**
+	 * 根据文件路径上传
+	 * 
+	 * @param remotePath
+	 *            FTP路径
+	 * @param remoteFile
+	 *            FTP文件
+	 * @param input
+	 *            本地输入流
+	 */
+	public static void upload(String remotePath, String remoteFile,
+			InputStream input) {
+		try {
+			if (remotePath != null && remotePath.length() != 0) {
+				ftpClient.changeWorkingDirectory(remotePath);
+			}
+			upload(remoteFile, input);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			logger.error("upload error", ex);
 		}
 	}
 
