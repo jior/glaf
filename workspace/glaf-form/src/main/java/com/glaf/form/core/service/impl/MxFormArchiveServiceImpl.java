b3c8fef9b385f7face2bdcf35aa312b87fdfc337
jior <jior2008@gmail.com>
2014-09-23 17:02:32 +0800

    update form service

--- workspace/glaf-form/src/main/java/com/glaf/form/core/service/impl/MxFormArchiveServiceImpl.java
+++ workspace/glaf-form/src/main/java/com/glaf/form/core/service/impl/MxFormArchiveServiceImpl.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.glaf.form.core.service.impl.mybatis;
+package com.glaf.form.core.service.impl;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
