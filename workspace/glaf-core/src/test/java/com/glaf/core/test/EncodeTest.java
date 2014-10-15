63b25278076068471b9a7f04511f4105f3f0c3a5
jior <jior2008@gmail.com>
2014-10-15 16:26:41 +0800

    update EncodeTest

--- workspace/glaf-core/src/test/java/com/glaf/core/test/EncodeTest.java
+++ workspace/glaf-core/src/test/java/com/glaf/core/test/EncodeTest.java
@@ -18,17 +18,25 @@
 
 package com.glaf.core.test;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
+import com.glaf.core.config.SystemConfig;
 import com.glaf.core.util.RequestUtils;
 import com.glaf.core.util.UUID32;
 
 public class EncodeTest {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 10000; i++) {
+		for (int i = 0; i < 100; i++) {
 			String str = RequestUtils.encodeString(String.valueOf(i) + "_"
 					+ UUID32.getUUID());
 			System.out.println(RequestUtils.decodeString(str));
 		}
+		System.out.println();
+		System.out.println(RequestUtils.encodeString("20140805/lfm-0000001"));
+		System.out.println(RequestUtils.decodeString("4b6a68716c64614e75724a4448353538514b57323065724536556d65614a6d324e497665594f6d493452614232446a415353656969734b526d426a41306b697175365536393150716264513d"));
+		String salt = DigestUtils.md5Hex(SystemConfig.getToken());
+		System.out.println(salt);
 	}
 
 }
